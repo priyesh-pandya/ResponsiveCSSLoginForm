268b4b884761033d283dff9261f6500e0b96ac69
Priyesh <priyesh.ak38@gmail.com>
2020-05-30 20:52:11 +0530

    Initial checkout

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function funcFocus(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remFocus(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", funcFocus);
+	input.addEventListener("blur", remFocus);
+});
+
